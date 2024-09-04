291163983d09fbd21cd5ef99ae8d49663e6d9d6a
Rahul kumar <rahulkrgupta18032003@gmail.com>
2024-09-04 15:09:09 +0530

    updating_configuration

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const allowedOrigins = [
 
 app.use(cors({
   origin: function(origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
+    console.log('Request Origin:', origin); // Log the origin
     if (!origin) return callback(null, true);
     if (allowedOrigins.indexOf(origin) === -1) {
       var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
